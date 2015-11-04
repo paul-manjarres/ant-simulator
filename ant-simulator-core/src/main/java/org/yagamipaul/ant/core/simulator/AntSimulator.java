019f51a9560ec5ad60475b9d848b25745723f808
Paul Manjarres <paul.manjarres@gmail.com>
2015-11-04 16:26:58 -0500

    Se agregan archivos iniciales del proyecto ant simulator core

---
+++ ant-simulator-core/src/main/java/org/yagamipaul/ant/core/simulator/AntSimulator.java
@@ -0,0 +1,22 @@
+/**
+ * Evoki.co - Plume Works Copyright (c) 2013 - 2015 by Jean Paul Manjarres Correal - All Rights
+ * Reserved. Propietary and Confidential - This source code is not for redistribution
+ * http://evoki.co Date: 4/11/2015
+ *
+ */
+package org.yagamipaul.ant.core.simulator;
+
+/**
+ * @author <a href="mailto:paul.manjarres@gmail.com">Jean Paul Manjarres Correal. </a> 4/11/2015
+ *
+ */
+public class AntSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
